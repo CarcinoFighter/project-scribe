045e8ab8d5376331729521ccd731ee1c561a2367
Naitik-Chattaraj <naitikchattaraj41@gmail.com>
2026-03-24 00:12:37 +0530

    build errors fixed ig

--- src/lib/usePushSubscription.ts
+++ src/lib/usePushSubscription.ts
@@ -48,7 +48,7 @@ export function usePushSubscription() {
         // Subscribe
         const subscription = await registration.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+          applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as any,
         });
 
         // Send to server
